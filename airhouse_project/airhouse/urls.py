6be45dbda77011e69390b8dea217212bfa969847
leo9uo <leo.g9oo@outlook.com>
2024-02-06 22:47:00 -0800

    regist form in progress

--- airhouse_project/airhouse/urls.py
+++ airhouse_project/airhouse/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index
+from .views import Index, SignUpView
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('register/', SignUpView.as_view(), name='signup'),
 ]
