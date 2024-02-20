35857e5d9119aad8ac67d61bb080f260ad0dd739
leo9uo <leo.g9oo@outlook.com>
2024-02-20 15:01:56 -0800

    fixed DateInput import in orders filter

--- airhouse_project/airhouse/filters.py
+++ airhouse_project/airhouse/filters.py
@@ -3,12 +3,11 @@ from django.forms.widgets import TextInput, DateInput
 from .models import *
 
 class OrderFilter(FilterSet):
-    order_date = DateFilter(widget=DateInput(attrs={'placeholder': 'Order Date', 'class': 'form-control'}))
+    order_date = DateFilter(widget=DateInput(attrs={'type': 'date', 'placeholder': 'Order Date', 'class': 'form-control'}))
     recipient = CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Recipient', 'class': 'form-control'}))
     order_source = CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Order Source', 'class': 'form-control'}))
-    skus_ordered = CharFilter(field_name='skus_ordered__name', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'SKUs Ordered', 'class': 'form-control'}))
+    skus_ordered = CharFilter(field_name='skus_ordered__inventory_item__name', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'SKUs Ordered', 'class': 'form-control'}))
 
     class Meta:
         model = Order
         fields = ['order_date', 'status', 'payment', 'order_source', 'recipient', 'skus_ordered']
-
