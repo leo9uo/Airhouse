acc0e3a2fd3137c14687783c204148b52896cba5
leo9uo <leo.g9oo@outlook.com>
2024-02-19 16:02:42 -0800

    add dateinput to order filter

--- airhouse_project/airhouse/filters.py
+++ airhouse_project/airhouse/filters.py
@@ -1,8 +1,19 @@
 import django_filters
+from django import forms
 
 from .models import *
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class OrderFilter(django_filters.FilterSet):
+    # use for date field UI on orders filter
+    order_date = django_filters.DateFilter(field_name='order_date', widget=DateInput())
+    
     class Meta:
         model = Order
         fields = ['order_date', 'status', 'payment', 'order_source', 'recipient', 'skus_ordered']
+
+
+
+
