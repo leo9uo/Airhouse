a8d71b0546eb8355575146b6d07c28a6a70674cb
leo9uo <leo.g9oo@outlook.com>
2024-02-20 16:45:37 -0800

    built in distinct function added to skus filter to fix duplicate order response

--- airhouse_project/airhouse/filters.py
+++ airhouse_project/airhouse/filters.py
@@ -6,8 +6,11 @@ class OrderFilter(FilterSet):
     order_date = DateFilter(widget=DateInput(attrs={'type': 'date', 'placeholder': 'Order Date', 'class': 'form-control'}))
     recipient = CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Recipient', 'class': 'form-control'}))
     order_source = CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Order Source', 'class': 'form-control'}))
-    skus_ordered = CharFilter(field_name='skus_ordered__inventory_item__name', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'SKUs Ordered', 'class': 'form-control'}))
+    skus_ordered = CharFilter(method='filter_skus_ordered', widget=TextInput(attrs={'placeholder': 'SKUs Ordered', 'class': 'form-control'}))
 
     class Meta:
         model = Order
-        fields = ['order_date', 'status', 'payment', 'order_source', 'recipient', 'skus_ordered']
+        fields = ['order_date', 'status', 'payment', 'order_source', 'recipient']
+
+    def filter_skus_ordered(self, queryset, name, value):
+        return queryset.filter(order_items__inventory_item__name__icontains=value).distinct()
