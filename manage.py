c5ae31ac515b4b6bea70f814ffb6c8a14fc03c03
leo9uo <leo.g9oo@outlook.com>
2024-02-24 22:54:54 -0800

    still secretkey error, adjust dotenv

--- manage.py
+++ manage.py
@@ -2,10 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv  # Import dotenv
 
 def main():
     """Run administrative tasks."""
+    # Load environment variables from .env file at the project root
+    dotenv.load_dotenv(
+        os.path.join(os.path.dirname(__file__), '.env')
+    )
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airhouse_project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +21,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
