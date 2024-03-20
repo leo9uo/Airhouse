f37cfac6e0c7761d570bafa1a62883381702e7fc
leo9uo <leo.g9oo@outlook.com>
2024-03-20 16:20:26 -0700

    build and migrate CustomerProfile model

--- client_side/models.py
+++ client_side/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='customer_profile')
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    # Credit card information
+    credit_card_number = models.CharField(max_length=16)
+    expiration_date = models.DateField()
+    cvv = models.CharField(max_length=4)
+    
+
+    def __str__(self):
+        return f"{self.user.email}'s Customer Profile"
