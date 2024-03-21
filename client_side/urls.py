64b8e311f78eb5857e8cfca3d206999394a16947
leo9uo <leo.g9oo@outlook.com>
2024-03-21 16:15:45 -0700

    CLIENT: available inventory table added

--- client_side/urls.py
+++ client_side/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Index, SignUpView, LoginView
+from .views import Index, SignUpView, LoginView, AvailableInventoryListView
 from django.contrib.auth import views as auth_views
 
 app_name = 'customer'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='client/login.html'), name='login'),
+    path('inventory/', AvailableInventoryListView.as_view(), name='inventory'),
 ]
--- client_side/views.py
+++ client_side/views.py
@@ -5,11 +5,13 @@ from .forms import SignUpForm, CustomerProfileForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from airhouse.models import InventoryItem
 
 
 class Index(TemplateView):
     template_name = 'client/index.html'
+
 
 class LoginView(LoginView):
     template_name = 'client/login.html'
@@ -37,3 +39,12 @@ class SignUpView(View):
             login(request, user)
             return redirect('customer:index')  
         return render(request, 'signup.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
+class AvailableInventoryListView(ListView):
+    template_name = 'client/avail_inv_list.html'
+    model = InventoryItem
+    context_object_name = 'inventory_items'
+
+    def get_queryset(self):
+        return InventoryItem.objects.all()
