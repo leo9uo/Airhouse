8a67dcf03b589c344a72778c5f007c0fbb4ea6c3
leo9uo <leo.g9oo@outlook.com>
2024-03-31 10:38:31 -0700

    CLIENT: added Cart and CartItem models, ran migrations

--- client_side/models.py
+++ client_side/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from airhouse.models import CustomUser
+from airhouse.models import CustomUser, InventoryItem
 
 class CustomerProfile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='customer_profile')
@@ -15,3 +15,20 @@ class CustomerProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.email}'s Customer Profile"
+    
+class Cart(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='cart')
+    items = models.ManyToManyField(InventoryItem, through='CartItem')
+
+    def __str__(self):
+        return f"Cart for {self.user.email}"
+    
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    inventory_item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.inventory_item.name} in cart for {self.cart.user.email}"
+
