8c9dd54127b989e04820dc61e774585b07e971aa
leo9uo <leo.g9oo@outlook.com>
2024-04-05 21:20:48 -0700

    CLIENT: logout for client side working properly

--- client_side/urls.py
+++ client_side/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='client/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='client/logout.html'), name='logout'),
     path('inventory/', AvailableInventoryListView.as_view(), name='inventory'),
     path('cart/', CartList.as_view(), name='cart'),
     path('add-to-cart/<int:inventory_item_id>/', AddToCart.as_view(), name='add-to-cart'),
