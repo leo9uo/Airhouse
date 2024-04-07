e797579eab54f40402aac9ba16e964600881a4e3
leo9uo <leo.g9oo@outlook.com>
2024-04-06 22:26:10 -0700

    CLIENT: change filter placeholder for item to item name

--- client_side/filters.py
+++ client_side/filters.py
@@ -5,7 +5,7 @@ from airhouse.models import InventoryItem
 
 class InventoryFilter(FilterSet):
     company_name = CharFilter(field_name='user__company_name', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Company Name', 'class': 'form-control'}))
-    name = CharFilter(lookup_expr='istartswith', widget=TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}))
+    name = CharFilter(lookup_expr='istartswith', widget=TextInput(attrs={'placeholder': ' Item Name', 'class': 'form-control'}))
     quantity = CharFilter(lookup_expr='istartswith', widget=TextInput(attrs={'placeholder': 'Quantity', 'class': 'form-control'}))
     price = CharFilter(lookup_expr='istartswith', widget=TextInput(attrs={'placeholder': 'Price', 'class': 'form-control'}))
 
